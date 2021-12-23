e9f746aa90a98981da16bc2bca81153c2e13147a
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-22 23:31:01 -0500

    commit tree 4f503bae649491ca1774dca04d0d2d909f8f5053

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
+
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener('submit', e => {
+      e.preventDefault();
+      console.log(e)
+    //   e.target.children[1].value 
+    const input = document.querySelector('input#searchByID');
+
+    console.log(input.value)
+
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
